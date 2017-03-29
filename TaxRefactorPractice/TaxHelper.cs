f501b1c9226cd264671934b79b8af6ee659071ec
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-03-29 16:58:21 +0800

    將參數字典化

--- TaxRefactorPractice/TaxHelper.cs
+++ TaxRefactorPractice/TaxHelper.cs
@@ -1,45 +1,53 @@
-﻿namespace TaxRefactorPractice
+﻿using System.Collections.Generic;
+
+namespace TaxRefactorPractice
 {
     public class TaxHelper
     {
-        private const int _boundary1 = 540_000;
-        private const int _boundary2 = 1_210_000;
-        private const int _boundary3 = 2_420_000;
-        private const int _boundary4 = 4_530_000;
-        private const int _boundary5 = 10_310_000;
-        private const decimal _rate0 = 0.05m;
-        private const decimal _rate1 = 0.12m;
-        private const decimal _rate2 = 0.2m;
-        private const decimal _rate3 = 0.3m;
-        private const decimal _rate4 = 0.4m;
-        private const decimal _rate6 = 0.5m;
+        private class BoundaryRate
+        {
+            public short Level;
+            public decimal Boundary;
+            public decimal TaxRate;
+        }
+
+        private static readonly List<BoundaryRate> BoundaryRateTable = new List<BoundaryRate>
+        {
+            new BoundaryRate{ Level = 0, Boundary =          0 , TaxRate = 0.05m },
+            new BoundaryRate{ Level = 1, Boundary =    540_000 , TaxRate = 0.12m },
+            new BoundaryRate{ Level = 2, Boundary =  1_210_000 , TaxRate = 0.2m  },
+            new BoundaryRate{ Level = 3, Boundary =  2_420_000 , TaxRate = 0.3m  },
+            new BoundaryRate{ Level = 4, Boundary =  4_530_000 , TaxRate = 0.4m  },
+            new BoundaryRate{ Level = 5, Boundary = 10_310_000 , TaxRate = 0.5m  },
+            new BoundaryRate{ Level = 6, Boundary = long.MaxValue },
+        };
 
         public static decimal GetTaxResult(decimal income)
         {
             decimal result = 0;
-            if (income < _boundary1)
+            if (income < BoundaryRateTable[1].Boundary)
             {
-                result = income * _rate0;
+                result = income * BoundaryRateTable[0].TaxRate;
             }
-            else if (income < _boundary2)
+            else if (income < BoundaryRateTable[2].Boundary)
             {
-                result = _boundary1 * _rate0 + (income - _boundary1) * _rate1;
+                result = BoundaryRateTable[1].Boundary * BoundaryRateTable[0].TaxRate + (income - BoundaryRateTable[1].Boundary) * BoundaryRateTable[1].TaxRate;
             }
-            else if (income < _boundary3)
+            else if (income < BoundaryRateTable[3].Boundary)
             {
-                result = _boundary1 * _rate0 + (_boundary2 - _boundary1) * _rate1 + (income - _boundary2) * _rate2;
+                result = BoundaryRateTable[1].Boundary * BoundaryRateTable[0].TaxRate + (BoundaryRateTable[2].Boundary - BoundaryRateTable[1].Boundary) * BoundaryRateTable[1].TaxRate + (income - BoundaryRateTable[2].Boundary) * BoundaryRateTable[2].TaxRate;
             }
-            else if (income < _boundary4)
+            else if (income < BoundaryRateTable[4].Boundary)
             {
-                result = _boundary1 * _rate0 + (_boundary2 - _boundary1) * _rate1 + (_boundary3 - _boundary2) * _rate2 + (income - _boundary3) * _rate3;
+                result = BoundaryRateTable[1].Boundary * BoundaryRateTable[0].TaxRate + (BoundaryRateTable[2].Boundary - BoundaryRateTable[1].Boundary) * BoundaryRateTable[1].TaxRate + (BoundaryRateTable[3].Boundary - BoundaryRateTable[2].Boundary) * BoundaryRateTable[2].TaxRate + (income - BoundaryRateTable[3].Boundary) * BoundaryRateTable[3].TaxRate;
             }
-            else if (income < _boundary5)
+            else if (income < BoundaryRateTable[5].Boundary)
             {
-                result = _boundary1 * _rate0 + (_boundary2 - _boundary1) * _rate1 + (_boundary3 - _boundary2) * _rate2 + (_boundary4 - _boundary3) * _rate3 + (income - _boundary4) * _rate4;
+                result = BoundaryRateTable[1].Boundary * BoundaryRateTable[0].TaxRate + (BoundaryRateTable[2].Boundary - BoundaryRateTable[1].Boundary) * BoundaryRateTable[1].TaxRate + (BoundaryRateTable[3].Boundary - BoundaryRateTable[2].Boundary) * BoundaryRateTable[2].TaxRate + (BoundaryRateTable[4].Boundary - BoundaryRateTable[3].Boundary) * BoundaryRateTable[3].TaxRate + (income - BoundaryRateTable[4].Boundary) * BoundaryRateTable[4].TaxRate;
             }
             else
             {
-                result = _boundary1 * _rate0 + (_boundary2 - _boundary1) * _rate1 + (_boundary3 - _boundary2) * _rate2 + (_boundary4 - _boundary3) * _rate3 + (_boundary5 - _boundary4) * _rate4 + (income - _boundary5) * _rate6;
+                result = BoundaryRateTable[1].Boundary * BoundaryRateTable[0].TaxRate + (BoundaryRateTable[2].Boundary - BoundaryRateTable[1].Boundary) * BoundaryRateTable[1].TaxRate + (BoundaryRateTable[3].Boundary - BoundaryRateTable[2].Boundary) * BoundaryRateTable[2].TaxRate + (BoundaryRateTable[4].Boundary - BoundaryRateTable[3].Boundary) * BoundaryRateTable[3].TaxRate + (BoundaryRateTable[5].Boundary - BoundaryRateTable[4].Boundary) * BoundaryRateTable[4].TaxRate + (income - BoundaryRateTable[5].Boundary) * BoundaryRateTable[5].TaxRate;
             }
             return result;
         }
